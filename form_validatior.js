417edefb52d7b7d2a66c16d3eee98574e20970f5
Roy <yooniks9@gmail.com>
2019-10-25 18:30:34 +0800

    Update and rename email_valiadtion.js to form_validatior.js

--- form_validatior.js
+++ form_validatior.js
@@ -2,3 +2,7 @@ function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
+function isPhone(phone) {
+    var regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    return regex.test(phone);
+}
